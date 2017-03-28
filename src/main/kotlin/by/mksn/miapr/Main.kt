4699e2ce216297ea8ec4e2779907460c8fd01eca
Mikhail_Snitavets <mksn13@gmail.com>
2017-03-28 17:03:20 +0300

    Add basic implementation (not working)

--- src/main/kotlin/by/mksn/miapr/Main.kt
+++ src/main/kotlin/by/mksn/miapr/Main.kt
@@ -1,6 +1,31 @@
 package by.mksn.miapr
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.util.*
+import javax.imageio.ImageIO
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    println("Reading images...")
+    val images = (0..19).map { PictureImage("$it.jpg", if (it < 10) 1.0 else 0.0) }
+    println("Perceptron training...")
+    val perceptron = Perceptron(imagePixelCount = 100)
+    for (i in 1..100000) {
+        perceptron.train(images)
+    }
+
+    val image = BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB)
+    for (i in 0..9) {
+        for (j in 0..9) {
+            image.setRGB(i, j, Color.HSBtoRGB(0.5.toFloat(), 0.5.toFloat(), perceptron.weights[i * 10 + j].toFloat()))
+        }
+    }
+    ImageIO.write(image, "png", File("test.png"))
+
+
+    println("Weights: ${Arrays.toString(perceptron.weights)}")
+    println("Positive test: ${perceptron.checkImageProbability(images[0])}")
+    println("Negative test: ${perceptron.checkImageProbability(images[10])}")
 }
 
--- src/main/kotlin/by/mksn/miapr/Perceptron.kt
+++ src/main/kotlin/by/mksn/miapr/Perceptron.kt
@@ -1,1 +1,74 @@
 package by.mksn.miapr
+
+import java.io.File
+import java.util.*
+import javax.imageio.ImageIO
+
+val RANDOM = Random()
+fun Double.equalsWithPrecision(other: Double, precision: Double = 0.000001): Boolean
+        = Math.abs(this - other) < precision
+
+fun Double.sqr(): Double = this * this
+
+fun imageToPixelArray(fileName: String): DoubleArray {
+    val classLoader = Perceptron::class.java.classLoader
+    val file = File(classLoader.getResource(fileName)!!.file)
+    val image = ImageIO.read(file)
+    val pixels = DoubleArray(image.width * image.height)
+    for (x in 0..image.width - 1) {
+        for (y in 0..image.height - 1) {
+            pixels[x * image.width + y] = (if (image.getRGB(x, y) == 0xFFFFFFFF.toInt()) 0.0 else 1.0)
+        }
+    }
+    return pixels
+}
+
+data class PictureImage(val pixelArray: DoubleArray, val targetImageProbability: Double) {
+    constructor(fileName: String, targetImageProbability: Double) :
+            this(imageToPixelArray(fileName), targetImageProbability)
+
+    fun calculateWeightSum(weights: DoubleArray, bias: Double): Double {
+        var sum = 0.0
+        weights.indices.forEach { i ->
+            sum += weights[i] * pixelArray[i]
+        }
+        return sum + bias
+    }
+}
+
+class Perceptron(imagePixelCount: Int,
+                 val threshold: Double = 0.0,
+                 val iterationLimit: Int = 100,
+                 val learningRate: Double = 0.001) {
+
+    val weights = DoubleArray(imagePixelCount, { RANDOM.nextDouble() })
+    var bias = RANDOM.nextDouble()
+
+    fun train(trainingSet: List<PictureImage>) {
+        var localError: Double
+        var globalError: Double
+        var iterationIndex: Int = 0
+        var output: Int
+        do {
+            iterationIndex++
+            globalError = 0.0
+            trainingSet.forEach { image ->
+                output = calculateOutput(image)
+                localError = image.targetImageProbability - output
+                weights.indices.forEach {
+                    i ->
+                    weights[i] += (learningRate * localError * image.targetImageProbability)
+                }
+                bias += learningRate * localError
+
+                globalError += learningRate.sqr()
+            }
+        } while (globalError.equalsWithPrecision(0.0) && (iterationIndex < iterationLimit))
+    }
+
+    private fun calculateOutput(image: PictureImage) =
+            if (image.calculateWeightSum(weights, bias) >= threshold) 1 else 0
+
+    fun checkImageProbability(image: PictureImage): Boolean
+            = calculateOutput(image) == 1
+}
